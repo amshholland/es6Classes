0c6c942816ff40e94af6e51c5c42306545a454f3
Amber Holland <Amber.S.Creech@gmail.com>
2021-04-14 14:55:03 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+class Room(name, length, width) {
+    {
+        name = name;
+        length = length;
+        width = width;
+    }
+    getArea {
+
+    }
+    getPerimeter {
+
+    }
+}
+
+let room1 = Room("Sun", 30, 20);
+let room2 = Room("Green", 15, 20);
+
+console.log
